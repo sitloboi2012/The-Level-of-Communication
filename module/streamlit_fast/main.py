d7088a0407f460c1f12f117f52879ca88fe1a382
Huy Vo <huyvo6812@gmail.com>
2021-05-25 20:35:14 +0930

    Update the main file Streamlit, add on model in

--- module/streamlit_fast/main.py
+++ module/streamlit_fast/main.py
@@ -1,6 +1,27 @@
-import streamlit as st 
+from multiprocessing import Pipe
+from operator import index
+from matplotlib.pyplot import axis
+import sklearn
+import streamlit as st
+import re
+import texthero as hero
+import nltk
+import numpy as np
+import pandas as pd
+import joblib
+
+from locale import normalize
+from tokenize import String
+from unicodedata import normalize as unicode_normalize
 from abc import ABC, abstractmethod
 from typing import Any, Dict, List
+from nltk.tokenize import punkt, word_tokenize
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+nltk.download("stopwords")
 
 st.title("The Level Of Communication")
 
@@ -10,11 +31,27 @@ job_title = st.text_input("Job Title","junior software engineer")
 job_description = st.text_input("Job Description","UniSA Centre of Change and Complexity in Learning (C3L) is currently hiring a junior software dev with more than 1 year of experience")
 
 
-def preprocessing(text: str):
-    pass
+tempData = pd.DataFrame({"job_title":job_title, "job_description":job_description}, index=[0])
+    
+def shallowClean(text: str):
+    """
+    Performs text normalization using regex patterns
+    """
+    text = unicode_normalize("NFC", text)
+    text = text.lower()
+    text = re.sub("```(.|\n|\r)*?```", "", text)
+    text = re.sub(r"\s+", " ", text)
+    text = re.sub("[-_:/]", " ", text)
 
-def modelling(job_title: str, job_description: str):
-    return None
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"\.+", ".", text)
+    text = re.sub("[?!;…]", ".", text)
+    text = text.replace("\n", ".")
+    return text
+
+def deepClean(text: str):
+    cleanText = hero.clean(text)
+    return cleanText
 
 def visualize_result(result):
     pass
@@ -24,4 +61,51 @@ def checkingTitle(job_title: str):
 
 def checkingJobDescription(job_description):
     return job_description
+
+def model_JobLevel(transformTitle):
+    model = joblib.load("SVC_LevelCategory_Model.pkl")
+    
+    result = model.predict(transformTitle)
+
+    return result
+
+def model_JobCategories(transformDescription):
+    model = joblib.load("SVC_JobIndustry_Model.pkl")
+
+    result = model.predict(transformDescription)
+
+    return result
+
+
+if st.button("Result"):
+    tempData["job_title"] = deepClean(tempData["job_title"])
+    tempData["job_description"] = deepClean(tempData["job_description"])
+
+    resultLevel = model_JobLevel(tempData["job_title"])
+    if resultLevel[0] == 0:
+        st.write("Director")
+    elif resultLevel[0] == 1:
+        st.write("Junior")
+    elif resultLevel[0] == 2:
+        st.write("Manager")
+    elif resultLevel[0] == 3:
+        st.write("Senior")
+    else:
+        raise("Not regconize level")
+    
+
+    resultCategory = model_JobCategories(tempData["job_description"])
+    if resultCategory[0] == 0:
+        st.write("Admin")
+    elif resultCategory[0] == 1:
+        st.write("Educational")
+    elif resultCategory[0] == 2:
+        st.write("Finance")
+    elif resultCategory[0] == 3:
+        st.write("Healthcare")
+    elif resultCategory[0] == 4:
+        st.write("Information Technology")
+    else:
+        raise("Not regconize level")
+
 
